2368914fd7180dc5dfb72c2192d3a362b2a62bf9
GustavoRocha <rocha.gustavo@escolar.ifrn.edu.br>
2023-06-29 11:58:23 -0300

    atualizando banco de dados

--- DB_Scrpit/script.sql
+++ DB_Scrpit/script.sql
@@ -1,25 +1,27 @@
-create table tbClient
-(
-    idConta       int auto_increment
-        primary key,
-    login         varchar(20) not null,
-    senha         varchar(40) not null,
-    nome          varchar(60) not null,
-    email         varchar(60) not null,
-    weight        int         not null,
-    age           int         not null,
-    tipe_diabetes tinyint     not null
-);
+create database db_diaadia;
+use db_diaadia;
+create table tbUser(
+    id int auto_increment primary key not null,
+    userName varchar(20) not null ,
+    name varchar(60) not null ,
+    lastName varchar(60) not null ,
+    email varchar(60) not null ,
+    password varchar(60) not null,
+    tipeClient  int not null comment '1:Cliente, 2:Médico, 3:Clinica'
 
-create table tbMeasure_daily
-(
-    idConta        int      not null
-        primary key,
-    meas_Gly_today date     not null,
-    hours_meas     datetime not null,
-    entry_app      datetime not null,
-    constraint fk_idConta
-        foreign key (idConta) references tbClient (idConta)
 );
-
+create table tbPacient(
+    id int primary key,
+    tipeDiabets tinyint not null  comment '1: Diabetes tipe 1,2: Diabetes tipe 2, 3: Diabetes tipe 3(gestacional)',
+    medicoId int not null,
+    clinicaId int not null,
+    measureGlicToday date not null ,
+    measureHours datetime not null,
+    entreyApp datetime not null,
+    constraint fk_user foreign key (id) references tbUser(id)
+);
+create table medic(
+    id int primary key,
+    
+)
 
