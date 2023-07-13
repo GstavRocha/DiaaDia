b57853002552b87438cc910cb6f9b72fa773418e
GustavoRocha <rocha.gustavo@escolar.ifrn.edu.br>
2023-07-13 20:08:50 -0300

    primeira tela

--- Mobile/diadia/src/app/app-routing.module.ts
+++ Mobile/diadia/src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  }
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
 ];
 @NgModule({
   imports: [
