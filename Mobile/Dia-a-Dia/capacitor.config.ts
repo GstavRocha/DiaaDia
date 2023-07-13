6f6cadc2aab3b8c1cc51f1aee6a445367001170d
GustavoRocha <rocha.gustavo@escolar.ifrn.edu.br>
2023-07-13 12:57:27 -0300

    adicionando e estilizando

---
+++ Mobile/Dia-a-Dia/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Dia-a-Dia',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
