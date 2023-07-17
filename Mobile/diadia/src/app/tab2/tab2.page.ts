51cf23f1176f7fb4fa0c71cd60a755cd01da926b
GustavoRocha <rocha.gustavo@escolar.ifrn.edu.br>
2023-07-17 20:44:55 -0300

    terminando estilos graficos

--- Mobile/diadia/src/app/tab2/tab2.page.ts
+++ Mobile/diadia/src/app/tab2/tab2.page.ts
@@ -1,44 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NavController} from "@ionic/angular";
 import * as HighCharts from 'highcharts';
+import darkUnica from 'highcharts/themes/dark-unica';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
   myChart: any;
   constructor(public nvCtrl: NavController) {}
-  ionViewDidEnter() {
-    var myChart = HighCharts.chart('chartContainer',{
-      chart:{
-        style:{
-          color: 'white'
-        },
-        type: 'spline',
-        backgroundColor: 'transparent',
+
+  ngOnInit() {
+    darkUnica(HighCharts)
+   this.myChart=  HighCharts.chart('chartContainer',{
+      chart: {
+        type: 'column'
       },
-      title: {
-        text: 'Frutas'
+      title:{
+        text: 'Indices Glicêmicos da Semana'
       },
       xAxis:{
-        categories: ['Maça', 'bananas', 'laranjas']
+        categories:[ 'Segunda-Feira 26/06', 'Terça-Feira 27/06', 'Quarta-Feira 28/06', 'Quinta-Feira 29/06', 'Sexta-Feira 30/06', 'Sábado 01/07', 'Domingo 02/07' ]
       },
-      yAxis: {
+      yAxis:{
+        min:0,
+        max: 150,
         title: {
-          text: 'Fruit Eaten'
+          text: 'Glicose'
+        }
+      },
+      tooltip:{
+        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage:.0f}%)',
+        shared: true
+      },
+      plotOptions:{
+        column: {
+          stacking: 'percent'
         }
       },
       series: [{
-        name:'Jane',
-        type: 'line',
-        data: [1,0,4,10,13,15]
+        name: 'Glicose Alta',
+        type: 'column',
+        data: [100,150,110,98,99,110,150]
       },{
-        name: 'Gustavo',
-        type: 'line',
-        data: [5,7,3,10,4,5,6]
+        name: 'Glicose Elevada',
+        type: 'column',
+        data: [110,115,130,134,114,116,125]
+      },{
+        name: 'Glicose Controlada',
+        type: 'column',
+        data: [100,100,100,99,98, 97, 99]
       }]
     })
+    };
   }
-}
+
