75663b8df919b6aac81955add98b6b08b55e386d
GustavoRocha <rocha.gustavo@escolar.ifrn.edu.br>
2023-07-31 20:07:59 -0300

    atividade

--- Mobile/diadia/src/app/tab1/tab1-routing.module.ts
+++ Mobile/diadia/src/app/tab1/tab1-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Tab1Page } from './tab1.page';
 import {Tab2Page} from "../tab2/tab2.page";
+import {Tab5Page} from "../tab5/tab5.page";
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'tab2',
     component: Tab2Page
+  },
+  {
+    path: 'tab5',
+    component: Tab5Page
   }
 ]
 
